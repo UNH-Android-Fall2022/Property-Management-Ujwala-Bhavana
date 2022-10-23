dd810eaa85c9892498174d82f1b315b6c23929c6
uyerra12 <98996518+uyerra12@users.noreply.github.com>
2022-10-23 16:50:20 -0400

    Added bottom navigation elements

--- app/src/main/java/com/example/property_management/MainActivity.kt
+++ app/src/main/java/com/example/property_management/MainActivity.kt
@@ -3,12 +3,18 @@ package com.example.property_management
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.inputmethod.InputBinding
+import android.widget.RadioButton
+import android.widget.RadioGroup
+import android.widget.Toast
+import androidx.core.view.get
 import com.example.property_management.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    val TAG = "Testing"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -20,5 +26,23 @@ class MainActivity : AppCompatActivity() {
             var intent = Intent(this, SignupActivity::class.java)
             startActivity(intent)
         }
+
+        val rg : RadioGroup = binding.radiogrp
+        //val checkedId = rg.checkedRadioButtonId
+
+        binding.btnLogin.setOnClickListener{
+            Log.d(TAG,"Login button clicked")
+            if(binding.rbtnowner.isChecked) {
+                //Log.d(TAG,"Owner radio button checked")
+                val intent = Intent(this,PropertiesActivity::class.java)
+                startActivity(intent)
+            }
+
+            else if(binding.rbtntenant.isChecked)
+                Log.d(TAG,"Tenant radio button checked")
+            else
+                Toast.makeText(applicationContext, "Select Owner or Tenant", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
