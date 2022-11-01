2c471f0124e8243b615871ccf0aceece908a2ea7
bhavana15512@gmail.com <bhavana15512@gmail.com>
2022-11-01 19:22:39 -0400

    Added navigation bar

--- app/src/main/java/com/example/tenantview_android_f22/ui/chat/ChatFragment.kt
+++ app/src/main/java/com/example/tenantview_android_f22/ui/chat/ChatFragment.kt
@@ -1,4 +1,4 @@
-package com.example.tenantview_android_f22.ui.notifications
+package com.example.tenantview_android_f22.ui.chat
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.tenantview_android_f22.databinding.FragmentNotificationsBinding
+import com.example.tenantview_android_f22.databinding.FragmentDashboardBinding
 
-class NotificationsFragment : Fragment() {
+class ChatFragment : Fragment() {
 
-    private var _binding: FragmentNotificationsBinding? = null
+    private var _binding: FragmentDashboardBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class NotificationsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val notificationsViewModel =
-            ViewModelProvider(this).get(NotificationsViewModel::class.java)
+        val dashboardViewModel =
+            ViewModelProvider(this).get(ChatViewModel::class.java)
 
-        _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
+        _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textChat
-        notificationsViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textDashboard
+        dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
