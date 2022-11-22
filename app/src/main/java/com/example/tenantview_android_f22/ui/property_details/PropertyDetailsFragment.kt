bf0f61c1d72b9cf2709c31d63a892f6d4b799623
bhavana15512@gmail.com <bhavana15512@gmail.com>
2022-11-22 11:31:50 -0500

    Modified property details fragment by adding db call to fetch the data from db

--- app/src/main/java/com/example/tenantview_android_f22/ui/property_details/PropertyDetailsFragment.kt
+++ app/src/main/java/com/example/tenantview_android_f22/ui/property_details/PropertyDetailsFragment.kt
@@ -1,14 +1,17 @@
 package com.example.tenantview_android_f22.ui.property_details
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.tenantview_android_f22.databinding.FragmentPropertyDetailsBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlin.math.log
 
 class PropertyDetailsFragment : Fragment() {
 
@@ -30,7 +33,30 @@ class PropertyDetailsFragment : Fragment() {
 
         _binding = FragmentPropertyDetailsBinding.inflate(inflater, container, false)
         val root: View = binding.root
+        var propertyName = ""
+        Log.d(TAG,"Calling the Property Details database...")
+        db.collection("Property_Details_Test")
+            .get()
+            .addOnSuccessListener { listOfProperties ->
+                for (property in listOfProperties){
+                    Log.d(TAG,"${property.id} => ${property.data}")
 
+                    val address1: TextView = binding.textAddress1Value
+                    address1.text = property.data["propertyName"].toString()
+
+                    val city: TextView = binding.textCityValue
+                    city.text = property.data["city"].toString()
+
+                    val state: TextView = binding.textStateValue
+                    state.text = property.data["state"].toString()
+
+                    val zipCode: TextView = binding.textZipCodeValue
+                    zipCode.text = property.data["zipCode"].toString()
+                }
+            }
+            .addOnFailureListener{ exception ->
+                Log.w(TAG,"Error getting documents", exception)
+            }
         return root
     }
 
