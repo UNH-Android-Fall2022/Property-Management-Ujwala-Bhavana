4e2d9c4b830b654ddd5b9f6f48002da8319095d3
bhavana15512@gmail.com <bhavana15512@gmail.com>
2022-11-30 13:07:21 -0500

    Added notification feature

--- app/src/main/java/com/example/tenantview_android_f22/ui/notifications/NotificationsFragment.kt
+++ app/src/main/java/com/example/tenantview_android_f22/ui/notifications/NotificationsFragment.kt
@@ -1,11 +1,19 @@
 package com.example.tenantview_android_f22.ui.notifications
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.getSystemService
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.tenantview_android_f22.R
 import com.example.tenantview_android_f22.databinding.FragmentNotificationsBinding
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
@@ -19,6 +27,8 @@ class NotificationsFragment : Fragment() {
     private val binding get() = _binding!!
     private val TAG = "Property_Management"
     private val db = Firebase.firestore
+    private val channelId = "channel_id_01"
+    private val notificationId = 101
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,10 +40,35 @@ class NotificationsFragment : Fragment() {
 
         _binding = FragmentNotificationsBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
+        createNotificationChannel()
+        binding.sendButton.setOnClickListener{
+            sendNotification()
+        }
         return root
     }
-
+    private fun createNotificationChannel(){
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Notification Title"
+            val descriptionText = "Notification Description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel : NotificationChannel = NotificationChannel(channelId,name,importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager = context?.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+    private fun sendNotification(){
+        var builder = NotificationCompat.Builder(requireContext(),channelId)
+            .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+            .setContentTitle("My notification")
+            .setContentText("Much longer text that cannot fit one line...")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        with(NotificationManagerCompat.from(requireContext())){
+            notify(notificationId,builder.build())
+        }
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
